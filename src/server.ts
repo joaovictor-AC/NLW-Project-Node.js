d6df693f9885d24263ca063279e18165c0deee7e
João Victor <jvaraujocosta13@gmail.com>
2021-06-23 15:37:38 -0300

    AULA 02

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
-import express, { request, response } from "express"
+import "reflect-metadata";
+import express from "express"
+import { router } from "./routes";
+import "./database"
 
 const app = express();
 
+app.use(express.json());
+
+app.use(router);
+
 app.listen(3000, () => console.log("Serve is running!!!"));
-
-app.get("/get", (request, response) => {
-    return response.send("GET");
-});
-
-app.post("/post" , (request, response) => {
-    return response.send("POST");
-});
